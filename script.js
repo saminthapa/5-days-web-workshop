014e141bcfbefe520f6e94b2e3eba80b2b75896f
Samin Thapa Kshetry <136580949+saminthapa@users.noreply.github.com>
2025-12-25 20:32:31 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,104 @@
+//variables
+
+// let a =5;
+// let b= "samin";
+
+// console.log(a);
+// console.log(b);
+// console.warn(b);
+// console.error(b);
+
+
+//flow control -> conditional statement
+
+// let a= 5;
+// if (a>2){
+//     console.log("Greater");
+// }else{
+//     console.log("Smaller")
+// }
+
+
+// loop
+
+// for(let i =1; i<3; i++){
+//     console.log(i);
+// }
+
+//function -> block of code
+
+// function greet(a){
+//     console.log("Good Morning",a);
+// }
+
+// greet("Ram");
+
+//popup box
+
+// alert("samin");
+// confirm("Are you sure?");
+// let name = prompt("Tero name k ho?");
+// alert(name);
+
+// Object -> store data -> key-value pairs
+
+// let student = {
+//     name: "samin", 
+//     address: "Bharatpur",
+//     clz: "Birendra Multiple Campus",
+//     faculty: "BIT",
+// }
+
+// console.log(student.name);
+// document.write(student.name +  "<br>");
+// document.write(student.clz);
+
+// array -> starts from 0
+
+// let student = [
+//     "Samin", 
+//     "Ram",
+//     "Hari"
+//  ];
+
+//  document.write(student[2]);
+
+
+// function validate(){
+//     let name = document.getElementById("name").value;
+//     let email = document.getElementById("email").value;
+//     let password = document.getElementById("password").value;
+
+// //validation
+   
+//    if (!name || name.length < 8){
+//        alert("Your Name must be at least 8 char.");
+//        return false;
+//    }
+
+//    if (!email.includes("@")){
+//        alert("Invalid Email");
+//        return false;
+//    }
+
+//    if(!password || password.length < 6){
+//     alert("Invalid Password");
+//     return false;
+//    }
+
+//    alert("Form Submitted successfully.")
+//    return true;
+// }
+
+//Event Handling
+
+//Error Handling -> try catch
+
+// try {
+//     let x = y + 10;
+// } catch (error) {
+//     alert(error.message)
+// }
+
+
+
